a505ab910842f847a70dd30430e248cc214e3af9
Edu <edu@rocket.localdomain>
2016-04-11 22:06:47 +0200

    Modifies Particle's constructor method

--- src/lib/particle.js
+++ src/lib/particle.js
@@ -3,14 +3,24 @@ var Particle = {
     velocity: null,
     gravity: null,
 
-    create: function(x, y, speed, direction, gravity) {
-        var particle = Object.create(this);
-        particle.position = Vector.create(x, y);
-        particle.velocity = Vector.create(0, 0);
-        particle.gravity = Vector.create(0, gravity);
+    create: function(particleSettings) {
+        particleSettings = arguments[0] || {};
+        particleSettings = {
+            x: particleSettings.x || 100,
+            y: particleSettings.y || 100,
+            speed: particleSettings.speed || 0,
+            direction: particleSettings.direction || 0,
+            gravity: particleSettings.gravity || 0
+        };
 
-        particle.velocity.setLength(speed);
-        particle.velocity.setAngle(direction);
+        var particle = Object.create(this);
+
+        particle.position = Vector.create(particleSettings.x, particleSettings.y);
+        particle.velocity = Vector.create(0, 0);
+        particle.gravity = Vector.create(0, particleSettings.gravity);
+
+        particle.velocity.setLength(particleSettings.speed);
+        particle.velocity.setAngle(particleSettings.direction);
 
         return particle;
     },
