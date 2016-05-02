5cfd21a5fac8d679ebb0d9ae39bf12a7b3695530
Edu <edu@rocket.localdomain>
2016-05-02 04:25:05 +0200

    New gravity method implemented
    The new gravity calculation method is based in attaction between
    particles making possible to simulate orbits.

--- src/lib/Particle.js
+++ src/lib/Particle.js
@@ -2,16 +2,16 @@ var Vector = Vector || require('./Vector');
 var Particle = {
     position: null,
     velocity: null,
-    gravity: null,
+    mass: null,
 
     create: function(particleSettings) {
         particleSettings = arguments[0] || {};
         particleSettings = {
-            x: particleSettings.x || 100,
-            y: particleSettings.y || 100,
+            x: particleSettings.x || 0,
+            y: particleSettings.y || 0,
             speed: particleSettings.speed || 0,
             direction: particleSettings.direction || 0,
-            gravity: particleSettings.gravity || 0
+            mass: particleSettings.mass || 1
         };
 
         var particle = Object.create(this);
@@ -20,7 +20,7 @@ var Particle = {
         particle.velocity = Vector.create({ x: 0, y: 0 });
         particle.velocity.setLength(particleSettings.speed);
         particle.velocity.setAngle(particleSettings.direction);
-        particle.gravity = Vector.create({ x: 0, y: particleSettings.gravity });
+        particle.mass = particleSettings.mass;
 
         return particle;
     },
@@ -31,7 +31,26 @@ var Particle = {
 
     update: function() {
         this.position.addTo(this.velocity);
-        this.accelerate(this.gravity);
+    },
+
+    angleTo: function(particle) {
+        return Math.atan2(particle.position.getY() - this.position.getY(), particle.position.getX() - this.position.getX());
+    },
+
+    distanceTo: function(particle) {
+        var dx = particle.position.getX() - this.position.getX();
+        var dy = particle.position.getY() - this.position.getY();
+        return Math.sqrt(dx * dx + dy * dy);
+    },
+
+    gravitateTo: function(particle) {
+        var gravity = Vector.create();
+        var distance = this.distanceTo(particle);
+        var effect = particle.mass / (distance * distance);
+        var angle = this.angleTo(particle);
+        gravity.setLength(effect);
+        gravity.setAngle(angle);
+        this.velocity.addTo(gravity);
     }
 };
 
