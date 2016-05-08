5a6a9feadce6b0b3de9fb8b53a11433fd3719523
Edu <edu@rocket.localdomain>
2016-05-09 00:55:31 +0200

    Added collition detection and fixed argument order bug for inRange
    Collition detection may be moved to a specific class soon.

--- src/lib/Utils.js
+++ src/lib/Utils.js
@@ -1,13 +1,44 @@
+'use strict';
+
 var Utils = {
+
     distance: function(p0, p1) {
         var dx = p0.x - p1.x;
         var dy = p0.y - p1.y;
         return Math.sqrt(dx * dx + dy * dy);
     },
-    distanceXY(x0, x1, y0, y1) {
-        var dx = x0 - x1;
-        var dy = y0 - y1;
+
+    distanceXY: function(x0, y0, x1, y1) {
+        var dx = x1 - x0;
+        var dy = y1 - y0;
         return Math.sqrt(dx * dx + dy * dy); 
+    },
+
+    inRange: function(value, min, max) {
+        return value >= Math.min(min, max) && value <= Math.max(min, max);
+    },
+
+    rangeIntersect: function(min0, max0, min1, max1) {
+        return  Math.max(min0, max0) >= Math.min(min1, max1) &&
+                Math.min(min0, max0) <= Math.max(min1, max1);
+    },
+
+    circleCollition: function(c0, c1) {
+        return Utils.distance(c0, c1) <= c0.radius + c1.radius;
+    },
+
+    circlePointCollition: function(px, py, circle) {
+        return Utils.distanceXY(px, py, circle.x, circle.y) < circle.radius;
+    },
+
+    rectanglePointCollition: function(px, py, rect) {
+        return  Utils.inRange(px, rect.x, rect.x + rect.width) &&
+                Utils.inRange(py, rect.y, rect.y + rect.height);
+    },
+
+    rectangleCollition: function(r0, r1) {
+        return  Utils.rangeIntersect(r0.x, r0.x + r0.width, r1.x, r1.x + r1.width) &&
+                Utils.rangeIntersect(r0.y, r0.y + r0.height, r1.y, r1.y + r1.height);
     }
 };
 
