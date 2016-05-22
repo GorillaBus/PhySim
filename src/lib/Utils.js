2e3858bd2ac76c7d1807e962c8fedf44b8606b49
Edu <edu@rocket.localdomain>
2016-05-23 01:43:46 +0200

    Fixed typo for 'collision' != 'collition' on Utils

--- src/lib/Utils.js
+++ src/lib/Utils.js
@@ -23,20 +23,24 @@ var Utils = {
                 Math.min(min0, max0) <= Math.max(min1, max1);
     },
 
-    circleCollition: function(c0, c1) {
+    randomRange: function(min, max) {
+        return min + Math.random() * (max - min);
+    },
+
+    circleCollision: function(c0, c1) {
         return Utils.distance(c0, c1) <= c0.radius + c1.radius;
     },
 
-    rectangleCollition: function(r0, r1) {
+    rectangleCollision: function(r0, r1) {
         return  Utils.rangeIntersect(r0.x, r0.x + r0.width, r1.x, r1.x + r1.width) &&
                 Utils.rangeIntersect(r0.y, r0.y + r0.height, r1.y, r1.y + r1.height);
     },
 
-    circlePointCollition: function(px, py, circle) {
+    circlePointCollision: function(px, py, circle) {
         return Utils.distanceXY(px, py, circle.x, circle.y) < circle.radius;
     },
 
-    rectanglePointCollition: function(px, py, rect) {
+    rectanglePointCollision: function(px, py, rect) {
         return  Utils.inRange(px, rect.x, rect.x + rect.width) &&
                 Utils.inRange(py, rect.y, rect.y + rect.height);
     },
