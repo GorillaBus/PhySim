fcd46fc4c33a4e6a6566bec844a59affb7be0fa6
Edu <edu@localhost.localdomain>
2016-06-09 10:41:52 +0200

    Added Bezier Curves to utils (quadratic and cubic)

--- src/lib/Utils.js
+++ src/lib/Utils.js
@@ -2,6 +2,24 @@
 
 var Utils = {
 
+    lerp : function(norm, min, max) {
+        return (max - min) * norm + min;
+    },
+
+    quadraticBezier: function(p0, p1, p2, t, pFinal) {
+        pFinal = pFinal || {};
+        pFinal.x = Math.pow(1 - t, 2) * p0.x + (1 - t) * 2 * t * p1.x + t * t * p2.x;
+        pFinal.y = Math.pow(1 - t, 2) * p0.y + (1 - t) * 2 * t * p1.y + t * t * p2.y;
+        return pFinal;
+    },
+
+    cubicBezier: function(p0, p1, p2, p3, t, pFinal) {
+        pFinal = pFinal || {};
+        pFinal.x = Math.pow(1 - t, 3) * p0.x + Math.pow(1 - t, 2) * 3 * t * p1.x + (1 - t) * 3 * t * t * p2.x + t * t * t * p3.x;
+        pFinal.y = Math.pow(1 - t, 3) * p0.y + Math.pow(1 - t, 2) * 3 * t * p1.y + (1 - t) * 3 * t * t * p2.y + t * t * t * p3.y;
+        return pFinal;
+    },
+
     distance: function(p0, p1) {
         var dx = p0.x - p1.x;
         var dy = p0.y - p1.y;
@@ -43,7 +61,7 @@ var Utils = {
     rectanglePointCollision: function(px, py, rect) {
         return  Utils.inRange(px, rect.x, rect.x + rect.width) &&
                 Utils.inRange(py, rect.y, rect.y + rect.height);
-    },
+    }
 };
 
 module.exports = Utils;
