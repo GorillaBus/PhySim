cbc0b45b340170301b5af03ea1c7b2f3d1509ae6
Edu <edu@localhost.localdomain>
2016-06-09 17:59:12 +0200

    Added 2.5d demo

---
+++ src/demos/js/2.5d.js
@@ -0,0 +1,76 @@
+var player;
+
+window.onload = function() {
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    var width = canvas.width = window.innerWidth-4;
+    var height = canvas.height = window.innerHeight-4;
+
+    var fl = 5500;
+    var shapes = [];
+    var numShapes = 5000;
+
+    for (var i=0;i<numShapes;i++) {
+        shapes[i] = {
+            x: Utils.randomRange(-1000, 1000),
+            y: Utils.randomRange(-1000, 1000),
+            z: Utils.randomRange(0, 100000)
+        };
+    }
+
+    // Demo player
+    player = AnimationPlayer.create();        
+    player.setUpdateFn(update);
+    player.play();
+
+
+    // New 0,0 position (vanish point)
+    ctx.translate(width/2, height/2);
+
+
+    /** Frame drawing function **/
+
+    function update(updateFn) {
+        ctx.clearRect(-width/2, -height/2, width, height);
+
+        for (var i=0;i<numShapes;i++) {            
+            var perspective = fl / (fl + shapes[i].z);
+
+            ctx.save();
+
+            ctx.translate(shapes[i].x * perspective, shapes[i].y * perspective);
+            ctx.scale(perspective, perspective);
+            ctx.beginPath();
+            ctx.arc(shapes[i].x, shapes[i].y, 5, Math.PI * 2, false);
+            ctx.fillStyle = "#FFFFFF"; 
+            ctx.fill();
+            ctx.closePath();
+            ctx.restore();
+
+            shapes[i].z += 250;
+            if (shapes[i].z > 99999) {
+                shapes[i].z = 0;
+            }
+        }
+    }
+    
+    /** Events **/
+
+    // Animation control: KeyDown
+    document.body.addEventListener("keydown", function(e) {
+        //console.log("Key pressed: ", e.keyCode);
+        switch (e.keyCode) {
+            case 27:                        // Esc
+                if (player.playing) {
+                    player.stop();
+                    console.log("> Scene stopped");
+                } else {
+                    player.play();
+                    console.log("> Playing scene");
+                }
+                break;
+            default:
+                break;
+        }
+    });    
+};
