954cd25ac684d527828f2928a9f24c734fe1d607
Edu <eduugr@gmail.com>
2016-10-12 17:24:45 +0200

    Impemented 'FEATURE TOGGLING' scheme and FPS control (AnimationPlayer)

---
+++ src/feature-toggle.js
@@ -0,0 +1,13 @@
+/*
+ *  Feature Toggling
+ *
+ *  Activete/Hide features that are in process of development or under testing
+ *  Once a feature is accepted to be includded must be removed from the
+ *  feature toggle scheme
+ */
+
+ const FEATURE_TOGGLE = {
+   FPS_CONTROL: true                // FPS controll for AnimationPlayer class
+ };
+
+ export default FEATURE_TOGGLE;
--- src/lib/AnimationPlayer.js
+++ src/lib/AnimationPlayer.js
@@ -1,9 +1,21 @@
+import FEATURE_TOGGLE from '../../src/feature-toggle';
+
 export default class AnimationPlayer {
 
     constructor(windowElement) {
         this.window = windowElement || window;
         this.requestId = null;
         this.playing = false;
+
+        // FPS control
+        if (FEATURE_TOGGLE.FPS_CONTROL) {
+          this.fps = 3;
+          this.now;
+          this.lastTime = Date.now();
+          this.interval = 1000/this.fps;
+          this.delta;
+        }
+
     }
 
     play() {
@@ -24,7 +36,21 @@ export default class AnimationPlayer {
         this.updateFn = (
             () => {
                 this.requestId = this.window.requestAnimationFrame(this.updateFn);
+
+                // FPS control
+                if (FEATURE_TOGGLE.FPS_CONTROL) {
+                  this.now = Date.now();
+                  this.delta = this.now - this.lastTime;
+
+                  if (this.delta > this.interval) {
+                    this.lastTime = this.now - (this.delta % this.interval);
+                    updateFn();
+                  }
+                  return;
+                }
+
                 updateFn();
+
             });
     }
 
